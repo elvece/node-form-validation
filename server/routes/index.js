a80420b024e640a5ef11031eb1912cffce06a685
elvece <lvcifferello@gmail.com>
2015-08-22 21:03:14 -0600

    added a few test routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Pups and Peeps' });
 });
+
+router.get('/new', function(req, res, next){
+  res.render('puppy.new', {
+    title: 'Add a Puppy'
+  });
+});
+
+router.get('/submit', function(req, res, next){
+  res.render('new', {
+    Title: 'Add a Puppy',
+  });
+});
+
+
+
+
+
+
 
 module.exports = router;
