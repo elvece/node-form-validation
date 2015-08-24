5356dc8eefebc6cd51e06cfd2bd151c12de108d4
elvece <lvcifferello@gmail.com>
2015-08-24 12:46:17 -0600

    added puppy validation and button to add another puppy

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
+var utility = require("../utility.js");
 var puppies = [];
+var people = [];
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Pups and Peeps' });
@@ -16,7 +17,8 @@ router.get('/puppy/new', function(req, res, next){
 router.get('/puppy/puppies', function(req, res, next){
   res.render('puppy/puppies', {
     Title: 'All the Puppies',
-    puppies: puppies
+    puppies: puppies,
+    success: 'The puppy was saved successfully.'
   });
 });
 
@@ -27,15 +29,24 @@ router.post('/submit', function(req, res, next){
 });
 
 router.post('/puppy/new', function(req, res, next){
-  puppies.push({
-    name: req.body.puppyName,
-    id: req.body.puppyId
-  });
-  res.redirect('/puppy/puppies', {
-    success: 'The puppy was successfully saved.'
-  });
-});
+  var puppyName = req.body.puppyName;
+  var puppyId = req.body.puppyId;
+  var errors = utility.puppyValidate(puppyName, puppyId);
 
+  if (errors.length > 0){
+    res.render('puppy/new', {
+      Title: 'Add a Puppy',
+      errors: errors
+    });
+  }
+  else {
+    puppies.push({
+      name: puppyName,
+      id: puppyId
+    });
+    res.redirect('/puppy/puppies');
+  }
+});
 
 
 
---
+++ server/utility.js
@@ -0,0 +1,36 @@
+
+function puppyValidate(puppyName, puppyId) {
+  var errorArray = [];
+  var puppyNameTrimmed = puppyName.trim(); //trims whitespace
+  var puppyIdTrimmed = puppyId.trim();
+
+  if(puppyNameTrimmed === '') {
+    errorArray.push("Name cannot be blank.");
+  }
+  if(puppyIdTrimmed === '') {
+    errorArray.push('ID cannot be blank.');
+  }
+  else if (puppyIdTrimmed.length < 3) {
+    errorArray.push('ID must be at least 3 characters long.');
+  }
+  return errorArray;
+}
+
+function personValidate(firstName, hobby) {
+  var errorArray = [];
+  var firstNameTrimmed = firstName.trim(); //trims whitespace
+  var hobbyTrimmed = hobby.trim();
+
+  if(firstNameTrimmed === '') {
+    errorArray.push("Name cannot be blank.");
+  }
+  else if(hobbyTrimmed === '') {
+    errorArray.push('Hobby cannot be blank.');
+  }
+  return errorArray;
+}
+
+module.exports = {
+  puppyValidate: puppyValidate,
+  personValidate: personValidate
+};
