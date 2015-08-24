5fd3a75b1b2b41ee88a7d37725891dbdd6c26b1f
elvece <lvcifferello@gmail.com>
2015-08-24 11:00:01 -0600

    added all puppy pages; can add puppies to table

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,24 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+var puppies = [];
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Pups and Peeps' });
 });
 
-router.get('/new', function(req, res, next){
-  res.render('puppy.new', {
-    title: 'Add a Puppy'
+router.get('/puppy/new', function(req, res, next){
+  res.render('puppy/new', {
+    Title: 'Add a Puppy'
   });
 });
 
-router.get('/submit', function(req, res, next){
-  res.render('new', {
+router.get('/puppy/puppies', function(req, res, next){
+  res.render('puppy/puppies', {
+    Title: 'All the Puppies',
+    puppies: puppies
+  });
+});
+
+router.post('/submit', function(req, res, next){
+  res.render('puppy/new', {
     Title: 'Add a Puppy',
   });
 });
 
-
-
+router.post('/puppy/new', function(req, res, next){
+  puppies.push({
+    name: req.body.puppyName,
+    id: req.body.puppyId
+  });
+  res.redirect('/puppy/puppies', {
+    success: 'The puppy was successfully saved.'
+  });
+});
 
 
 
